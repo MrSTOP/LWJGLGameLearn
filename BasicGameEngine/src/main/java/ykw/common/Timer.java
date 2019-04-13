5431c6c9edc42b06b9fd38fe93e452e87ea351bc
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-04-13 22:02:06 +0800

    添加Timer类

---
+++ BasicGameEngine/src/main/java/ykw/common/Timer.java
@@ -0,0 +1,24 @@
+package ykw.common;
+
+public class Timer {
+    private double lastLoopTime;
+
+    public void init() {
+        lastLoopTime = getTime();
+    }
+
+    public double getTime() {
+        return System.nanoTime();
+    }
+
+    public float getElapsedTime() {
+        double time = getTime();
+        float elapsedTime = (float) (time - lastLoopTime);
+        lastLoopTime = time;
+        return elapsedTime;
+    }
+
+    public double getLastLoopTime() {
+        return lastLoopTime;
+    }
+}
