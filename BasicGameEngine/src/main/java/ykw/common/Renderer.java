dbdb969e8b6f42a3fd6c1d997cf49cc72e2945c6
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-04-13 21:37:33 +0800

    添加Renderer类

--- BasicGameEngine/src/main/java/ykw/common/Renderer.java
+++ BasicGameEngine/src/main/java/ykw/common/Renderer.java
@@ -1,4 +1,14 @@
 package ykw.common;
 
-public class Render {
+import org.lwjgl.opengl.GL11;
+
+public class Renderer {
+
+    public void init() throws Exception {
+
+    }
+
+    public void clear() {
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+    }
 }
